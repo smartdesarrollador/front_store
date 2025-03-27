01e8eb3b603b21623661f8acf3e726fa98165e29
Jeans <sistemadesignstyle@gmail.com>
2025-03-27 00:15:33 -0500

    Iniciando

---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  /* URL_BACKEND: 'https://apiaws.tiendaperuvian.com/',
+  URL_SERVICIOS: 'https://apiaws.tiendaperuvian.com/api',
+  URL_FRONTED: 'https://tiendaaws.tiendaperuvian.com', */
+  URL_BACKEND: 'https://apitienda.soporteonlineperu.com/',
+  URL_SERVICIOS: 'https://apitienda.soporteonlineperu.com/api',
+  URL_FRONTED: 'https://tienda.soporteonlineperu.com',
+  production: true,
+};
