9af56b22e73d42fe6ef586c347370d75abffadf7
Jeans <sistemadesignstyle@gmail.com>
2025-03-29 18:56:48 -0500

    actualizando rutas

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,8 @@ export const environment = {
   /* URL_BACKEND: 'https://apiaws.tiendaperuvian.com/',
   URL_SERVICIOS: 'https://apiaws.tiendaperuvian.com/api',
   URL_FRONTED: 'https://tiendaaws.tiendaperuvian.com', */
-  URL_BACKEND: 'https://apitienda.soporteonlineperu.com/',
-  URL_SERVICIOS: 'https://apitienda.soporteonlineperu.com/api',
-  URL_FRONTED: 'https://tienda.soporteonlineperu.com',
+  URL_BACKEND: 'https://apitiendamaquimotora.enfocussoluciones.pe/',
+  URL_SERVICIOS: 'https://apitiendamaquimotora.enfocussoluciones.pe/api',
+  URL_FRONTED: 'https://tiendamaquimotora.enfocussoluciones.pe',
   production: true,
 };
